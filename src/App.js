d9c84ce7b743af8bb8e3df40acac54a40cea722c
Onewoo <wbw220@naver.com>
2021-09-19 18:11:37 +0900

    Fix components
    nochi on iOS and statusbar on android no longer covers the components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import styled, { ThemeProvider } from 'styled-components/native';
 import { theme } from './theme';
 
-const Container = styled.View`
+const Container = styled.SafeAreaView`
     flex: 1;
     background-color: ${({ theme }) => theme.background};
     align-items: center;
     justify-content: flex-start;
-`;
+`; // SafeAreaView로 아이폰 노치 디자인 문제 해결
 
 const Title = styled.Text`
     font-size: 40px;
@@ -21,8 +22,12 @@ export default function App() {
     return (
         <ThemeProvider theme={theme}>
             <Container>
+                <StatusBar
+                    barStyle="light-content"
+                    backgroundColor={theme.background}
+                />
                 <Title>TODO List</Title>
             </Container>
         </ThemeProvider>
     );
-}
+}// StatusBar를 이용하여 상태 바와 컴포넌트가 겹치지 않게 함
