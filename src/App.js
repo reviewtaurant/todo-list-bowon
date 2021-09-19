2cd2580deefc5210eb156f08b8b965f113f6abd3
Onewoo <wbw220@naver.com>
2021-09-19 22:41:28 +0900

    Add Task Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, {useState} from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, Dimensions } from 'react-native';
 import styled, { ThemeProvider } from 'styled-components/native';
 import { theme } from './theme';
 import Input from './components/Input';
-import { images } from './images';
-import IconButton from './components/IconButton';
+import Task from './components/Task';
 
 const Container = styled.SafeAreaView`
     flex: 1;
@@ -20,6 +19,13 @@ const Title = styled.Text`
     align-self: flex-start;
     margin: 0px 20px;
 `;
+
+const List = styled.ScrollView`
+    flex: 1;
+    width: ${({ width }) => width - 40}px;
+`; //ScrollView를 이용해 리스트가 많아져도 스크롤로 확인 가능
+
+const width = Dimensions.get('window').width;
 
 export default function App() {
     const [newTask, setNewTask] = useState(''); //tast 변수의 추가와 setter 함수 추가
@@ -47,10 +53,12 @@ export default function App() {
                     onChangeText={_handleTextChange}
                     onSubmitEditing={_addTask} 
                 />
-                <IconButton type={images.uncompleted} />
-                <IconButton type={images.completed} />
-                <IconButton type={images.delete} />
-                <IconButton type={images.update} />
+                <List width={width}>
+                    <Task text="Hanbit" />
+                    <Task text="React Native" />
+                    <Task text="React Native Sample" />
+                    <Task text="Edit TODO Item" />
+                </List>
             </Container>
         </ThemeProvider>
     );
---
+++ src/components/Task.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import PropTypes from 'prop-types';
+import IconButton from './IconButton';
+import { images } from '../images';
+
+const Container = styled.View`
+    flex-direction: row;
+    align-items: center;
+    background-color: ${({ theme }) => theme.itemBackground};
+    border-radius: 10px;
+    padding: 5px;
+    margin: 3px 0px;
+ `;
+
+ const Contents = styled.Text`
+    flex: 1;
+    font-size: 24px;
+    color: ${({ theme }) => theme.text};
+`;
+
+const Task = ({ text }) => { // props로 할 일 내용이 전달됨
+    return (
+        <Container>
+            <IconButton type={images.uncompleted} />
+            <Contents>{text}</Contents>
+            <IconButton type={images.update} />
+            <IconButton type={images.delete} />
+        </Container>
+    ); //완료, 추가, 삭제 버튼이 추가됨
+};
+
+Task.propTypes = {
+    text: PropTypes.string.isRequired,
+};
+
+export default Task;
